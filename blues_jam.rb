519714ea65759cc71bd80bd80202b60ae6c7d9e4
Metallion <akieckens@axsh.net>
2018-03-07 09:56:20 +0900

    Fixed a mistake in the walkup to the 4th in blues jam

--- blues_jam.rb
+++ blues_jam.rb
@@ -5,7 +5,7 @@ in_thread(name: :conductor) do
   loop do
     set :key, key
     sleep 4
-
+    
     # Some times play the 4chord on the second bar
     if rand(0..1) == 0
       set :key, key + 5
@@ -13,12 +13,12 @@ in_thread(name: :conductor) do
       set :key, key
     end
     sleep 4
-
+    
     2.times do
       set :key, key
       sleep 4
     end
-
+    
     2.times do
       set :key, key + 5
       sleep 4
@@ -27,7 +27,7 @@ in_thread(name: :conductor) do
       set :key, key
       sleep 4
     end
-
+    
     set :key, key + 7
     sleep 4
     set :key, key + 5
@@ -57,7 +57,7 @@ end
 in_thread(name: :bass) do
   use_synth :blade
   root = key
-
+  
   def simple_shuffle_bass(notes)
     4.times do
       note = notes.tick
@@ -67,22 +67,22 @@ in_thread(name: :bass) do
       sleep 1/3.0
     end
   end
-
+  
   define :major_chord_arpeggio do
     simple_shuffle_bass (ring root, root + 4, root + 7, root + 4)
     root = sync :key
   end
-
+  
   define :walkup_to_4th do
-    simple_shuffle_bass (ring root, root + 1, root + 2, root + 3)
+    simple_shuffle_bass (ring root, root + 2, root + 3, root + 4)
     root = sync :key
   end
-
+  
   define :walkup_to_5th do
     simple_shuffle_bass (ring root, root + 4, root + 5, root + 6)
     root = sync :key
   end
-
+  
   define :turnaround_5th do
     play root
     sleep 1
@@ -90,18 +90,18 @@ in_thread(name: :bass) do
     play root, sustain: (2 + 1/3.0)
     root = sync :key
   end
-
+  
   loop do
     3.times { major_chord_arpeggio }
-
+    
     if rand(0..1) == 0
       major_chord_arpeggio
     else
       walkup_to_4th
     end
-
+    
     6.times { major_chord_arpeggio }
-
+    
     walkup_to_5th
     turnaround_5th
   end
@@ -110,7 +110,7 @@ end
 in_thread(name: :guitar) do
   use_synth :pluck
   root = key
-
+  
   loop do
     play chord(root, :major7), amp: 1.5
     sleep 1
